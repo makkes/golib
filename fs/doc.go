7108d38131a63fd1274a9f6b96a477e333fce5da
Max Jonas Werner <mail@makk.es>
2021-02-18 14:15:37 +0100

    docs: added package documentation; go mod tidy

---
+++ fs/doc.go
@@ -0,0 +1,3 @@
+// Package fs contains utility functions to interact with filesystems, mainly using the io/fs package introduced
+// in Go 1.16.
+package fs
---
+++ try/doc.go
@@ -0,0 +1,3 @@
+// Package try contains utility functions to repeatedly execute certain functions until a condition is met (e.g.
+// a timeout occurred).
+package try
