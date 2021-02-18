0d00ebf8563aae4990d0c5b63820e6a1ae64765b
Max Jonas Werner <mail@makk.es>
2021-02-18 12:00:39 +0100

    feat: added CopyFSWithOptions func

--- fs/fs.go
+++ fs/fs.go
@@ -6,17 +6,39 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-// CopyFS recursively copies the complete content of the given fs.FS object into the directory pointed to by out.
-// The function will return immediately as soon as an error occurs which may leave the output directory in an
-// incomplete state compared to in.
-func CopyFS(in fs.FS, out string) error {
+type config struct {
+	stripComponents int
+}
+
+// Option is used to change the default behaviour of this package's functions.
+type Option func(c *config)
+
+// StripComponents strips n leading components from the input FS when copying its content.
+func StripComponents(n int) Option {
+	return func(c *config) {
+		c.stripComponents = n
+	}
+}
+
+// CopyFSWithOptions is like CopyFS but with the possibility to change its behaviour.
+func CopyFSWithOptions(in fs.FS, out string, opts ...Option) error {
+	c := &config{}
+	for _, opt := range opts {
+		opt(c)
+	}
 	if err := fs.WalkDir(in, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return fmt.Errorf("error: %w", err)
 		}
-		p := filepath.Join(out, path)
+		parts := strings.Split(path, string(filepath.Separator))
+		if len(parts) <= c.stripComponents {
+			return nil
+		}
+		strippedPath := filepath.Join(parts[c.stripComponents:]...)
+		p := filepath.Join(out, strippedPath)
 		if d.IsDir() {
 			if err := os.MkdirAll(p, 0750); err != nil {
 				return fmt.Errorf("could not create dir %s: %w", p, err)
@@ -39,4 +61,11 @@ func CopyFS(in fs.FS, out string) error {
 		return err
 	}
 	return nil
+}
+
+// CopyFS recursively copies the complete content of the given fs.FS object into the directory pointed to by out.
+// The function will return immediately as soon as an error occurs which may leave the output directory in an
+// incomplete state compared to in.
+func CopyFS(in fs.FS, out string) error {
+	return CopyFSWithOptions(in, out)
 }
--- fs/fs_test.go
+++ fs/fs_test.go
@@ -9,17 +9,16 @@ import (
 
 	"go.e13.dev/golib/fs"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 //go:embed a
 var embFS embed.FS
 
-func TestUnpack(t *testing.T) {
+func TestCopyFS(t *testing.T) {
 	target := t.TempDir()
 	if err := fs.CopyFS(embFS, target); err != nil {
-		t.Fatalf("Unpack returned an unexpected error: %s", err)
+		t.Fatalf("CopyFS returned an unexpected error: %s", err)
 	}
 	filepath.Walk("a", func(path string, info os.FileInfo, err error) error {
 		require.NoError(t, err, "failed to walk test data files")
@@ -28,15 +27,63 @@ func TestUnpack(t *testing.T) {
 		}
 		relPath := path
 		unpackedPath := filepath.Join(target, relPath)
-		assert.FileExists(t, unpackedPath)
+		require.FileExists(t, unpackedPath)
 		originalFile, err := ioutil.ReadFile(path)
-		assert.NoError(t, err, "failed to read original file")
+		require.NoError(t, err, "failed to read original file")
 		unpackedFile, err := ioutil.ReadFile(unpackedPath)
-		assert.NoError(t, err, "failed to read unpacked file")
-		assert.Equal(t, originalFile, unpackedFile, "unpacked file is different from original")
+		require.NoError(t, err, "failed to read unpacked file")
+		require.Equal(t, originalFile, unpackedFile, "unpacked file is different from original")
 		fi, err := os.Lstat(unpackedPath)
-		assert.NoError(t, err, "failed to stat unpacked file")
-		assert.Equal(t, os.FileMode(0640), fi.Mode(), "wrong unpacked file mode on %s", fi.Name())
+		require.NoError(t, err, "failed to stat unpacked file")
+		require.Equal(t, os.FileMode(0640), fi.Mode(), "wrong unpacked file mode on %s", fi.Name())
 		return nil
 	})
+}
+
+func TestCopyFSWithStripComponents(t *testing.T) {
+	tests := map[string]struct {
+		strip         int
+		expectedFiles map[string][]byte
+	}{
+		"strip 0 components": {
+			0,
+			map[string][]byte{
+				"a/b/c.txt": []byte("This is C\n"),
+				"a/b/d.txt": []byte("This is D\n"),
+			},
+		},
+		"strip 1 component": {
+			1,
+			map[string][]byte{
+				"b/c.txt": []byte("This is C\n"),
+				"b/d.txt": []byte("This is D\n"),
+			},
+		},
+		"strip 2 components": {
+			2,
+			map[string][]byte{
+				"c.txt": []byte("This is C\n"),
+				"d.txt": []byte("This is D\n"),
+			},
+		},
+		"strip 3 components": {
+			3,
+			nil,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			target := t.TempDir()
+			if err := fs.CopyFSWithOptions(embFS, target, fs.StripComponents(tc.strip)); err != nil {
+				t.Fatalf("Unpack returned an unexpected error: %s", err)
+			}
+
+			for file, expectedContent := range tc.expectedFiles {
+				content, err := os.ReadFile(filepath.Join(target, file))
+				require.NoError(t, err, "file %s should exist and be readable", file)
+				require.Equal(t, expectedContent, content, "unpacked file is different from original")
+			}
+		})
+	}
 }
